7ac6154e69ba958d103fae9cbc593bdbedb52015
Giovanni <111045802+Giovi06@users.noreply.github.com>
2022-11-03 10:52:29 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
 
+namespace LA1200
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inPath = @"/Users/giovanni/Downloads/Ohne Titel.csv";
+            string text = File.ReadAllText(inPath);
+
+            string[] lines = text.Split("\r\n");
+            int students = lines.Length;
+            string[] mail = new string[students];
+            string[] first = new string[students];
+            string[] second = new string[students];
+            string[] third = new string[students];
+
+            string outText = "";
+
+            foreach (var student in first)
+            {
+                Random rnd = new Random();
+
+                switch (first[rnd.Next(0,students)])
+                {
+                    case string n when (n == "Rasenmähen"):
+
+                        break;
+
+                }
+            }
+
+
+            string outPath = @"/Users/giovanni/Downloads/Lösung2.csv";
+
+            File.WriteAllText(outPath, outText);
+        }
+    }
+}
